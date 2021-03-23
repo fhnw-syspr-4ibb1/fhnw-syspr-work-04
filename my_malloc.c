635679b3b7f2db491403d8091c3a760bdf13db29
Thomas Amberg <tamberg@yaler.net>
2021-03-23 14:31:46 +0100

    Update my_malloc.c

--- my_malloc.c
+++ my_malloc.c
@@ -1,27 +1,50 @@
+#define _DEFAULT_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <assert.h>
 
 void *my_malloc(size_t size) {
-    return malloc(size); // TODO: replace
+    return malloc(size); // TODO: implement with sbrk() and/or brk()
 }
 
 void my_free(void *p) {
-    free(p); // TODO: replace
+    free(p); // TODO: implement with sbrk() and/or brk()
 }
 
-void test() {
-    int n = 3;
-    int *p = my_malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++) {
-        printf("%p: %d\n", p, *p);
-        p++;
-    }
-    p -= n;
+void test1() {
+    int *p = my_malloc(sizeof(int));
+    int *q = my_malloc(sizeof(int));
+    int *r = my_malloc(sizeof(int));
+    printf("%p: %d\n", (void *) p, *p);
+    printf("%p: %d\n", (void *) q, *q);
+    printf("%p: %d\n", (void *) r, *r);
+    // call my_free() in reverse order
+    my_free(r);
+    my_free(q);
     my_free(p);
+    // assert memory has been freed
+    assert(sbrk(0) == (void *) p);
+}
+
+void test2() {
+    int *p = my_malloc(sizeof(int));
+    int *q = my_malloc(sizeof(int));
+    int *r = my_malloc(sizeof(int));
+    printf("%p: %d\n", (void *) p, *p);
+    printf("%p: %d\n", (void *) q, *p);
+    printf("%p: %d\n", (void *) r, *p);
+    // call my_free() in any order
+    my_free(p);
+    my_free(r);
+    my_free(q);
+    // assert memory has been freed
+    assert(sbrk(0) == (void *) p);
 }
 
 int main() {
-    test();
-    test();
-    test();
+    test1();
+    test2();
 }
